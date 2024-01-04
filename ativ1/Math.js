3f1b6b2fd00c951223a1ca04e95b06610b543253
Julia Marques <122115178+juliamambrozio@users.noreply.github.com>
2024-01-04 15:49:18 -0300

    math

---
+++ ativ1/Math.js
@@ -0,0 +1,4 @@
+const raio = 5.6;
+const area = Math.PI + Math.pow(raio,2) //Math: objeto para operaç~eos matemáticas, pow = faz o valor ser elevado o número informado, PI = informa o valor real de PI 
+
+console.log(area.toFixed(2))
