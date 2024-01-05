43d0d32a31b360da6dfc738a89e5a402623d1b66
Julia Marques <122115178+juliamambrozio@users.noreply.github.com>
2024-01-05 19:32:58 -0300

    hoisting

--- ativ2/var_let.js
+++ ativ2/var_let.js
@@ -28,4 +28,47 @@ for(let i = 0; i < 10; i++){
     console.log(i)
 }
 
+
+//OUTRO EXEMPLO
+
+const funcs = []
+
+for(var i = 0; i < 10; i++){
+    funcs.push(function(){
+        console.log(i)
+    })
+}
+
+funcs[8](); //retorna o último valor do laço
+funcs[2]();
+
+const funcs2 = []
+
+for(let i = 0; i < 10; i++){
+    funcs2.push(function(){
+        console.log(i)
+    })
+}
+
+funcs2[8](); //let tem uma espécia de memória que recupera o valor contido nesse intervalo
+funcs2[2]();
+
+
+//VAR E LET COM HOISTING
+
+console.log(a)//não exibe um erro, pois a var ela recebe um hoisting, ou seja, sua declaração é "criada" no começo do código
+var a = 2
+console.log(a) 
+
+
+console.log(a) //dá um erro, pois a var a não existe quando esse console foi declarado
+let a = 2
+console.log(a) 
+
+
+
+
+
+
+
 console.log("O valor da var é:" + i) //Não tem acesso ao let, pois ele respeita o escopo do bloco
