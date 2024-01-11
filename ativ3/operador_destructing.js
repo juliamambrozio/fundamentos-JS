1e62a5bf45aa65230f776e7bcf099bb5c8532817
Julia Marques <122115178+juliamambrozio@users.noreply.github.com>
2024-01-11 16:14:43 -0300

    destructing como parâmetro

--- ativ3/operador_destructing.js
+++ ativ3/operador_destructing.js
@@ -35,3 +35,53 @@ console.log(g, b, h)
 
 const [m, [p, o]] = [8, [5, 0]] //array dentro de array
 console.log(m, p, o)
+
+//PASSANDO COMO PARÂMETRO
+
+//OBJETO
+
+function rand({min = 0, max = 1000}){ //valor padrão
+    const valor = Math.random() * (max - min) + min //não precisa usar o ponto para acessar o objeto graças ao destructing
+    return Math.floor(valor)
+}
+
+const obj = {min: 40, max: 60}
+console.log(rand(obj))
+console.log(rand({min: 10})) //não passei o max, então ele pega o padrão que foi definido como mil
+
+//ARRAY
+
+function rand([min = 0, max = 1000]){ //valor padrão
+    if(min > max) [min, max] = [max, min] //minímo vira máximo e máximo vira mínimo
+    const valor = Math.random() * (max - min) + min 
+    return Math.floor(valor)
+}
+
+console.log(rand([10,5]))
+
+
+//PASSANDO COMO PARÂMETRO
+
+//OBJETO
+
+function rand({min = 0, max = 1000}){ //valor padrão
+    const valor = Math.random() * (max - min) + min //não precisa usar o ponto para acessar o objeto graças ao destructing
+    return Math.floor(valor)
+}
+
+const objeto = {min: 40, max: 60}
+console.log(rand(objeto))
+console.log(rand({min: 10})) //não passei o max, então ele pega o padrão que foi definido como mil
+
+//ARRAY
+
+function rand([min = 0, max = 1000]){ //valor padrão
+    if(min > max) [min, max] = [max, min] //minímo vira máximo e máximo vira mínimo
+    const valor = Math.random() * (max - min) + min 
+    return Math.floor(valor)
+}
+
+console.log(rand([10,5]))
+
+
+
