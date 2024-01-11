8b71bf6fd8597d2540ab14eb27debcd6bc580981
Julia Marques <122115178+juliamambrozio@users.noreply.github.com>
2024-01-11 16:11:37 -0300

    operador de atribuição

---
+++ ativ3/operador_atribuicao.js
@@ -0,0 +1,17 @@
+const a = 10
+let b = 7
+
+b += a //b = a + b
+console.log(b)
+
+b -= 2 //b = 2 + b
+console.log(b)
+
+b *= 2 //b = 2 * b
+console.log(b)
+
+b /= 2 //b = 2 / b
+console.log(b)
+
+b %= 2 //b = b % 2 //par = 0, ímpar = 1
+console.log(b)
